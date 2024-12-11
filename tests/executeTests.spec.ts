a056f87e85d986ad85114fdc52cf92b91080d97e
ShwetaLohar <shwetalohar014@gmail.com>
2024-12-11 12:50:20 +0530

    improved code

--- tests/executeTests.spec.ts
+++ tests/executeTests.spec.ts
@@ -1,31 +1,31 @@
-import { test } from '@playwright/test';
+// import { test } from '@playwright/test';
 
-const specFiles = [
-//   './registration.spec.ts',
-  './login.spec.ts',
-//   './onboardingForms.spec.ts',
-//   './checkerMerchant.spec.ts',
-//   './submerchantUpload.spec.ts',
-//   './checkerSubmerchant.spec.ts',
-//   './merchantActivation.spec.ts',
-//   './submerchantActivation.spec.ts',
-];
+// const specFiles = [
+// //   './registration.spec.ts',
+//   './login.spec.ts',
+// //   './onboardingForms.spec.ts',
+// //   './checkerMerchant.spec.ts',
+// //   './submerchantUpload.spec.ts',
+// //   './checkerSubmerchant.spec.ts',
+// //   './merchantActivation.spec.ts',
+// //   './submerchantActivation.spec.ts',
+// ];
 
-// Read tags from the environment variable or use an empty string if none is provided
-// const tags = process.env.TAGS || '';
+// // Read tags from the environment variable or use an empty string if none is provided
+// // const tags = process.env.TAGS || '';
 
-// // Define a regular expression to match the tags (like @smoke or @regression)
-// const grepRegex = new RegExp(tags.split(',').map(tag => `@${tag}`).join('|'), 'i');
+// // // Define a regular expression to match the tags (like @smoke or @regression)
+// // const grepRegex = new RegExp(tags.split(',').map(tag => `@${tag}`).join('|'), 'i');
 
-for (const specFile of specFiles) {
-  test.describe(`Running tests from ${specFile}`, () => {
+// for (const specFile of specFiles) {
+//   test.describe(`Running tests from ${specFile}`, () => {
 
-    test.beforeAll(async () => {
-    });
+//     test.beforeAll(async () => {
+//     });
 
-    test.afterAll(async () => {
-    });
+//     test.afterAll(async () => {
+//     });
 
-    require(specFile);
-  });
-}
+//     require(specFile);
+//   });
+// }
